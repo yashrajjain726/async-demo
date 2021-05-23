4464b8858b4ea10b78acd9e0286d7700ed645b46
Yashraj Jain <yashrajjain726@gmail.com>
2021-05-23 23:35:55 +0530

    promise implementation done

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-console.log('Before');
-getUser(1,displayUsers);
-console.log('after');
+console.log("Before");
+console.log("after");
 
+// Named functions to rescue callback rescue/ callback hell problem
+// function displayUsers(user){
+//     getRepositories(user.githubUsername,displayRepos);
+// }
+// function displayRepos(repos){
+//     console.log('Repo List',repos)
+// }
 
-// Named functions to rescue callback rescue
-function displayUsers(user){
-    console.log('User',user);
-    getRepositories(user.githubUsername,displayRepos);
-}
-function displayRepos(repos){
-    console.log('Repo List',repos)
-}
-
-
+const p = getUser(1)
+  .then((user) => getRepositories(user.githubUsername))
+  .then((repos) => console.log(repos))
+  .catch((err) => console.log(err.message));
 
 // callbacks functions
-function getUser(id,callback){
-    setTimeout(()=>{
-        console.log('Reading user from the database....');
-        callback({id:id,githubUsername:'yashrajjain726'});
-    },2000)
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading user from the database....");
+      resolve({ id: id, githubUsername: "yashrajjain726" });
+    }, 2000);
+  });
 }
 
-function getRepositories(username,callback){
-    setTimeout(()=>{
-        console.log('Calling Github API');
-        callback(['repo1','repo2','repo3']);
-    },2000)
-    
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Calling Github API");
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
---
+++ promise-api.js
@@ -0,0 +1,14 @@
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Operation 1");
+    resolve(1);
+  }, 2000);
+});
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Operation 1");
+    resolve(2)
+  }, 2000);
+});
+
+Promise.all([p1,p2]).then(result=>console.log(result));
